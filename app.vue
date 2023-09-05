5af1c43bb6b32a6872444ea4bbc94bcc5fb24101
cheatcode <dwikurnia.cheatcode@gmail.com>
2023-09-05 15:45:06 +0700

    store firestore

--- app.vue
+++ app.vue
@@ -1,5 +1,28 @@
+<script setup>
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import {initializeApp} from "firebase/app";
+import {getFirestore, collection, query, where, getDocs} from "firebase/firestore";
+//declare const process:{env:{FIREBASE_CONFIG:string}};
+const db = getFirestore(initializeApp(JSON.parse(process.env.FIREBASE_CONFIG)));
+
+import { ref, reactive } from "vue"
+const ye = ref(true);
+
+const alldoc = ref(new Object());
+(await getDocs(query(collection(db, 'peminjaman', ),where('kembali','==',null)))).forEach(doc=>{
+  alldoc.value[doc.id]=doc.data()
+});
+</script>
+
 <template>
-  <div>
+  <div v-if="ye">
+    <ol v-for="(data, id) in alldoc">
+      <li>{{ data }}</li>
+    </ol>
+  </div>
+  <div v-else>
     <NuxtWelcome />
   </div>
 </template>
