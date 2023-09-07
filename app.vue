27bd04ad9e4f51e1fbb0570f26cd91325cd129a4
cheatcode <dwikurnia.cheatcode@gmail.com>
2023-09-07 14:09:28 +0700

    env suddenly breaks why

--- app.vue
+++ app.vue
@@ -1,78 +1,19 @@
-<script setup>
-import {initializeApp} from "firebase/app";
-import {getFirestore, collection, query, where, getDocs} from "firebase/firestore";
-//declare const process:{env:{FIREBASE_CONFIG:string}};
-const db = getFirestore(initializeApp(JSON.parse(process.env.FIREBASE_CONFIG)));
+<script setup lang="ts">
+import { ref } from "vue"
+const ye = ref(true)
 
-import { ref, reactive } from "vue"
-const ye = ref(true);
-
-const alldoc = ref(new Object());
-(await getDocs(query(collection(db, 'peminjaman', ),where('kembali','==',null)))).forEach(doc=>{
-  alldoc.value[doc.id]=doc.data()
-});
-
-function tulisanTgl(tgl){
-  const bulanAngkaKeHuruf = {
-    0:'Januari',
-    1:'Februari',
-    2:'Maret',
-    3:'April',
-    4:'Mei',
-    5:'Juni',
-    6:'Juli',
-    7:'Agustus',
-    8:'September',
-    9:'Oktober',
-    10:'November',
-    11:'Desember',
-  };
-  return `${tgl.getDate()} ${bulanAngkaKeHuruf[tgl.getMonth()]} ${tgl.getFullYear()}`;
-}
-
-class simpleTgl {
-  tanggal;
-  bulan;
-  tahun;
-
-  constructor(tgl){
-    this.tanggal = tgl.getDate();
-    this.bulan = 1+tgl.getMonth();
-    this.tahun = tgl.getFullYear();
-  }
-
-  toString(){
-    return `${this.tanggal} ${simpleTgl.#bulanAngkaKeHuruf[this.bulan]} ${this.tahun}`;
-  }
-
-  static #bulanAngkaKeHuruf = {
-    0:'Januari',
-    1:'Februari',
-    2:'Maret',
-    3:'April',
-    4:'Mei',
-    5:'Juni',
-    6:'Juli',
-    7:'Agustus',
-    8:'September',
-    9:'Oktober',
-    10:'November',
-    11:'Desember',
-  }
-}
-
-
+console.log(process.env.FIREBASE_CONFIG);
 </script>
 
 <template>
-  <div v-if="ye">
-    <ol>
-      <span v-for="(data, id) in alldoc">
-        <li>{{ data.peminjam }} meminjam "{{ data.buku }}" pada tanggal {{ data.pinjam!=null?tulisanTgl(data.pinjam.waktu.toDate()):'tak tertulis' }}</li>
-      </span>
-    </ol>
-  </div>
-  <div v-else>
-    <NuxtWelcome />
+  <div>
+    <div v-if="ye">
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </div>
+    <div v-else>
+      <NuxtWelcome />
+    </div>
   </div>
 </template>
--- pages/scan.vue
+++ pages/scan.vue
@@ -1,3 +1,12 @@
+<script setup>
+console.log(process.env.FIREBASE_CONFIG)
+/*
+import {initializeApp} from "firebase/app"
+import {getFirestore} from "firebase/firestore"
+const db = getFirestore(initializeApp(JSON.parse(process.env.FIREBASE_CONFIG)));
+*/
+</script>
+
 <template>
   <div>bro it works</div>
 </template>
