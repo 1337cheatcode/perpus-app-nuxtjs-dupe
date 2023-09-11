6de8aaf57c2c250294636bd2ac4086df98de33d6
cheatcode <dwikurnia.cheatcode@gmail.com>
2023-09-11 16:52:47 +0700

    yeah (integrating /scan with firestore)

--- pages/scan.vue
+++ pages/scan.vue
@@ -1,6 +1,6 @@
 <script setup lang="tsx">
 import {initializeApp} from "firebase/app"
-import {getFirestore} from "firebase/firestore"
+import {Firestore, Timestamp, addDoc, collection, getFirestore} from "firebase/firestore"
 declare namespace process { namespace env {const FIREBASE_CONFIG:string}};
 
 import QrScanner from 'qr-scanner';
@@ -12,38 +12,13 @@ const videoElement = ref<HTMLVideoElement>(),
       hasFlash = ref(false),
       cams = ref<QrScanner.Camera[]>([]),
       activeCamId = ref('');
-let qrScanner:QrScanner;
+let qrScanner:QrScanner, db:Firestore;
 
 watch(activeCamId, (id)=>qrScanner.setCamera(id));
 watch(text, ()=>navigator.vibrate(150))
 
 interface PinjamQRIntf {
 
-}
-
-function storeData(res:QrScanner.ScanResult){
-  const validRes = ()=>{
-    alert('yeah');
-    qrScanner.stop();
-  };
-
-  const ress = res.data.split(',');
-  switch (ress[0]) {
-    case 'p':
-      console.log(ress[1],ress[2]);
-      validRes();
-      break;
-    case 'x':
-      console.log(ress[1],ress[2]);
-      validRes();
-      break;
-    default:
-      console.log(res.data);
-      qrScanner.pause().then(()=>alert('try again'));
-      qrScanner.start();
-      break;
-  }
-  qrScanner.stop();
 }
 
 onMounted(async ()=>{
@@ -53,7 +28,7 @@ onMounted(async ()=>{
     cams.value = await QrScanner.listCameras(true);
     
     try {
-      const db = getFirestore(initializeApp(JSON.parse(useRuntimeConfig().public.FIREBASE_CONFIG)));
+      db = getFirestore(initializeApp(JSON.parse(useRuntimeConfig().public.FIREBASE_CONFIG)));
       qrScanner = new QrScanner(
         videoElement.value!,
         storeData,
@@ -76,6 +51,43 @@ onMounted(async ()=>{
       hasFlash.value = await qrScanner.hasFlash();
       activeCamId.value = cams.value[cams.value.length-1].id
     } catch (e) {}
+  }
+
+  async function storeData(res:QrScanner.ScanResult){
+    const validRes = ()=>{
+      qrScanner.stop();
+      alert('yeah');
+      useRouter().back();
+    };
+
+    const ress = res.data.split(',');
+    switch (ress[0]) {
+      case 'p':
+        console.log(ress[1],ress[2]);
+        addDoc(collection(db,'peminjaman'),{
+          peminjam:ress[1],
+          buku:ress[2],
+          waktu:{
+            pinjam:Timestamp.now(),
+            staf: 'webscan'
+          }
+        })
+        validRes();
+        break;
+      case 'x':
+        console.log(ress[1],ress[2]);
+        validRes();
+        break;
+      case 'k':
+        console.log(ress[1],ress[2]);
+        validRes();
+      default:
+        console.log(res.data);
+        qrScanner.pause().then(()=>alert('try again'));
+        qrScanner.start();
+        break;
+    }
+    qrScanner.stop();
   }
 });
 </script>
